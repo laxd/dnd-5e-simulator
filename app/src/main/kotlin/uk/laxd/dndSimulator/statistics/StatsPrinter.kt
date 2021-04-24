13705d00a0a24ced27c3bf3ee06b97d008696e6b
Lawrence Andrews <lawrence@lawrenceandrews.co.uk>
2021-04-24 20:12:36 +0100

    Refactored StatsPrinter to parse stats per character

--- app/src/main/kotlin/uk/laxd/dndSimulator/statistics/StatsPrinter.kt
+++ app/src/main/kotlin/uk/laxd/dndSimulator/statistics/StatsPrinter.kt
@@ -19,33 +19,34 @@ class StatsPrinter(private val eventLogger: EventLogger) {
             .distinct()
             .filterNotNull()
 
-        // Attacks per encounter
-        val attacksPerEncounter = getStats(characters,
+        for (character in characters) {
+            LOGGER.info("{} stats:", character.name)
+
+            // Attacks per encounter
+            val attacksPerEncounter = getStats(character,
                 eventLogger.events,
                 { x: EncounterEvent -> x.type == EncounterEventType.MELEE_ATTACK },
                 EncounterEventType.ENCOUNTER_START,
                 { 1 })
 
-        // Hits per encounter
-        val hitsPerEncounter = getStats(characters,
+            // Hits per encounter
+            val hitsPerEncounter = getStats(character,
                 eventLogger.events,
                 { x -> x is MeleeAttackEvent && (x.outcome == AttackOutcome.HIT || x.outcome == AttackOutcome.CRIT) },
                 EncounterEventType.ENCOUNTER_START,
                 { 1 })
 
-        // Damage by melee attacks per round
-        val meleeDamagePerRound = getStats(characters,
+            // Damage by melee attacks per round
+            val meleeDamagePerRound = getStats(character,
                 eventLogger.events,
                 { x -> x is MeleeAttackEvent && (x.outcome == AttackOutcome.HIT || x.outcome == AttackOutcome.CRIT) },
                 EncounterEventType.ROUND_START,
                 { x -> (x as MeleeAttackEvent).amount.totalAmount })
 
-        for (character in characters) {
-            LOGGER.info("{} stats:", character.name)
-            LOGGER.info("Attacks per encounter: {}", attacksPerEncounter[character])
-            LOGGER.info("Hits per encounter: {}", hitsPerEncounter[character])
-            LOGGER.info("Hit percent: {}%", 100 * (hitsPerEncounter[character]!!.sum / attacksPerEncounter[character]!!.sum.toDouble()))
-            LOGGER.info("Damage dealt per round: {}", meleeDamagePerRound[character])
+            LOGGER.info("Attacks per encounter: {}", attacksPerEncounter)
+            LOGGER.info("Hits per encounter: {}", hitsPerEncounter)
+            LOGGER.info("Hit percent: {}%", 100 * (hitsPerEncounter.sum / attacksPerEncounter.sum.toDouble()))
+            LOGGER.info("Damage dealt per round: {}", meleeDamagePerRound)
             LOGGER.info("\n")
         }
 
@@ -62,42 +63,33 @@ class StatsPrinter(private val eventLogger: EventLogger) {
         }
     }
 
-    private fun getStats(characters: Collection<Character>,
+    private fun getStats(character: Character,
                          events: Collection<EncounterEvent>,
                          filter: EncounterStatsFilter,
                          groupBy: EncounterEventType,
-                         extractor: EncounterStatsExtractor): Map<Character, IntSummaryStatistics> {
-        val statistics: MutableMap<Character, IntSummaryStatistics> = HashMap()
-        characters.forEach(Consumer { c: Character -> statistics[c] = IntSummaryStatistics() })
-        val ongoingTotal: MutableMap<Character, Int> = HashMap()
+                         extractor: EncounterStatsExtractor): IntSummaryStatistics {
+        val stats = IntSummaryStatistics()
+        var count = 0
+
         for (event in events) {
             // Loop over events, every time something matches the filter, add it to the total.
             // Add total to statistics every time a "ROUND_START" event happens
             if (event.type == groupBy) {
-                // Persist total and reset
-                for (character in ongoingTotal.keys) {
-                    val stats = statistics[character]
-                    stats!!.accept(ongoingTotal[character]!!)
-                    ongoingTotal[character] = 0
-                }
+                stats.accept(count);
+                count = 0
             }
-            if (filter.matches(event) && event.actor != null) {
-                var currentTotal = ongoingTotal[event.actor] ?: 0
-                
-                currentTotal += extractor.getAttribute(event)
-                ongoingTotal[event.actor] = currentTotal
+
+            if (filter.matches(event) && event.actor == character) {
+                count += extractor.getAttribute(event)
             }
         }
 
         // As all events has been taken into account, make sure to flush the current ongoingTotals
-        for (character in ongoingTotal.keys) {
-            val stats = statistics[character]
-            if (ongoingTotal[character] != 0) {
-                stats!!.accept(ongoingTotal[character]!!)
-                ongoingTotal[character] = 0
-            }
+        if (count != 0) {
+            stats.accept(count)
         }
-        return statistics
+
+        return stats
     }
 
     companion object {
